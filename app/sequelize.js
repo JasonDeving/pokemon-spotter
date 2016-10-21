bffe3c48779a9943a933d9871088b53e96bb8810
jasondeving <aznj50@hotmail.com>
2016-10-20 23:23:36 -0700

    to the cloud

--- app/sequelize.js
+++ app/sequelize.js
@@ -3,8 +3,8 @@ var Sequelize = require('sequelize'),
  //    	host: 'localhost',
  //    	dialect: 'mysql'
  //    })
-    sequelize = new Sequelize('o3quxqj2utbg0ueb', 'n5i36j4eb1w6ibx3', 'zid5fxowrlnixkid', {
-    	host: 'z37udk8g6jiaqcbx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    sequelize = new Sequelize('', '', '', {
+    	host: '',
     	dialect: 'mysql'
     })
 
